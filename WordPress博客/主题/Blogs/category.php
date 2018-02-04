a83ba097b1334a3e578dfdf93632922b75ef86ca
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-02-04 18:05:30 +0800

    【MySQL性能优化】概述与优化方面(一)

--- WordPress博客/主题/Blogs/category.php
+++
@@ -1,2 +0,0 @@
-<?php if (get_option('ygj_fllbbj') == '图片布局') {get_template_part('category-grid');} 
-else { get_template_part('archive');} ?>
--- WordPress博客/主题/Blogs/index.php
+++
@@ -1,3 +0,0 @@
-<?php if (get_option('ygj_home') == '杂志布局') {get_template_part('cms');} 
-else if(get_option('ygj_home') == '图片布局'){get_template_part('grid');}
-else { get_template_part('blog'); } ?>
